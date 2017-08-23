b0c189482b42224d10219f7cd0c933b844476575
18815288493 <minyuchun@163.com>
2017-08-23 17:20:13 +0800

    提交测试的工具类
    提交TestUtils的java工具类 来尝试都否可以被引用

---
+++ androidlibs/src/main/java/com/ist/androidlibs/TestUtils.java
@@ -0,0 +1,12 @@
+package com.ist.androidlibs;
+
+/**
+ * Created by minyuchun on 2017/8/23.
+ */
+
+public final class TestUtils {
+
+    public static String getName(){
+        return "test";
+    }
+}
